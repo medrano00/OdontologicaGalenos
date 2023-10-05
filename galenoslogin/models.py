287ef9bd9f657a0a9b6e856fe238dc39b69aa5dd
Fabián Espinoza M <faboespinozam@icloud.com>
2023-10-05 14:48:41 -0300

    FINALMENTEEEEE

--- galenoslogin/models.py
+++ galenoslogin/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Reserva(models.Model):
+    rut = models.CharField(max_length=20)
+    sucursal = models.CharField(max_length=100)
+    prevision = models.CharField(max_length=100)
+    especialidad = models.CharField(max_length=100)
+    fecha = models.DateField()
--- galenoslogin/views.py
+++ galenoslogin/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import ReservarCitaForm
-
+from .models import Reserva
 
 # Create your views here.
 
@@ -11,11 +11,14 @@ def reservarCita(request):
     if request.method == 'POST':
         form = ReservarCitaForm(request.POST)
         if form.is_valid():
-            rut = form.cleaned_data['rut']
-            sucursal = form.cleaned_data['sucursal']
-            prevision = form.cleaned_data['prevision']
-            especialidad = form.cleaned_data['especialidad']
-            fecha = form.cleaned_data['fecha']
+            reserva = Reserva(
+                rut = form.cleaned_data['rut'],
+                sucursal = form.cleaned_data['sucursal'],
+                prevision = form.cleaned_data['prevision'],
+                especialidad = form.cleaned_data['especialidad'],
+                fecha = form.cleaned_data['fecha']
+            )
+            reserva.save()
             return redirect('reservaRealizada')
     else:
         form = ReservarCitaForm()
