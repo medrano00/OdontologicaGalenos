885bd703adc1da6d92b82a52550a32723177c6bc
Fabián Espinoza M <62431500+medrano00@users.noreply.github.com>
2023-10-07 21:34:27 -0300

    a

--- galenos/tests.py
+++ galenos/tests.py
@@ -1,10 +1,8 @@
-from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from django.test import LiveServerTestCase
 
-# Create your tests here.
-
-class HostTest(LiveServerTestCase):
-    def testhomepage(self):
-        driver = webdriver.Chrome()
-        driver.get('http://127.0.0.1:8000/index/')
-        assert "Odontológica Galenos" in driver.title
+class LoginTest(LiveServerTestCase):
+def G01(self):
+    driver = webdriver.Chrome()
+    driver.
