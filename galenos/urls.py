2c1780f5ce78466239fccab7efa7e75251bdb96f
Fabián Espinoza M. <62431500+medrano00@users.noreply.github.com>
2023-10-10 17:58:07 -0300

    Pruebas en Selenium realizadas

--- galenos/urls.py
+++ galenos/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-from django.contrib.auth.views import LoginView
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('index/', views.index, name='index'),
@@ -10,3 +10,6 @@ urlpatterns = [
     path('', LogoutView.as_view(), name='logout'),
     path('reservarCita/', views.reservarCita, name='reservarCita'),
 ]
+
+# Include static file handlers for testing
+urlpatterns += staticfiles_urlpatterns()
