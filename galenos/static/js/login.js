d9b4e4d76741210c95c3ef77955d0fdd8567a6f7
Fabián Espinoza M <62431500+medrano00@users.noreply.github.com>
2023-10-14 01:51:15 -0300

    :D

--- galenos/static/js/login.js
+++ galenos/static/js/login.js
@@ -22,3 +22,15 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     };
     xhr.send('username=' + encodeURIComponent(username) + '&password=' + encodeURIComponent(password));
 });
+
+function validateForm() {
+    var username = document.getElementById("id_username").value;
+    var password = document.getElementById("id_password").value;
+    if (username == "" && password == "") {
+      alert("Campos vacíos, por favor ingresar información");
+    } else if (username != "fespinoza" || password != "fabo1234") {
+      alert("Campos erróneos, por favor corrija la información");
+    } else {
+      alert("Usuario ha iniciado sesión satisfactoriamente");
+    }
+  }
